1f633a8a42ca57092b1133b32102a10aa226c2ed
ankye sheng <shengxiehua@me.com>
2019-10-10 20:31:23 +0800

    添加call方法

--- server/call.go
+++ server/call.go
@@ -85,3 +85,17 @@ func GOWithContext(v ...interface{}) {
 		App.UpdateGoCount(-1)
 	}()
 }
+
+func Future(f func() (interface{}, error)) func() (interface{}, error) {
+	var result interface{}
+	var err error
+	c := make(chan struct{}, 1)
+	go func() {
+		defer close(c)
+		result, err = f()
+	}()
+	return func() (interface{}, error) {
+		<-c
+		return result, err
+	}
+}

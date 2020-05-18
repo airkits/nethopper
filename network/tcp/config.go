2f6a6f09ca8fa5802c141bccc8ccd07fa1084177
ankye sheng <shengxiehua@me.com>
2020-05-18 19:53:03 +0800

    修改配置文件

--- network/tcp/config.go
+++ network/tcp/config.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gonethopper/nethopper/network/common"
 )
 
-//ClientConfig grpc client config
+//ClientConfig tcp client config
 type ClientConfig struct {
 	Nodes            []common.NodeInfo `yarm:"nodes"`
 	ConnNum          int               `yarm:"conn_num"`
@@ -15,12 +15,22 @@ type ClientConfig struct {
 	MaxMessageSize   uint32            `yarm:"max_message_size"`
 	HandshakeTimeout time.Duration     `yarm:"handshake_timeout"`
 	AutoReconnect    bool              `yarm:"auto_reconnect"`
+	Network          string            `yarm:"network"`
+	Token            string            `yarm:"token"`
+	UID              uint64            `yarm:"uid"`
+	ReadBufferSize   int               `yarm:"read_buffer_size"`
+	WriteBufferSize  int               `yarm:"write_buffer_size"`
+	ReadDeadline     time.Duration     `yarm:"read_dead_line"`
 }
 
-//ServerConfig grpc server config
+//ServerConfig tcp server config
 type ServerConfig struct {
-	Address         string `yaml:"address"`
-	MaxConnNum      int    `yaml:"max_conn_num"`
-	SocketQueueSize int    `yaml:"socket_queue_size"`
-	MaxMessageSize  uint32 `yaml:"max_message_size"`
+	Address         string        `yaml:"address"`
+	Network         string        `yaml:"network"`
+	MaxConnNum      int           `yaml:"max_conn_num"`
+	SocketQueueSize int           `yaml:"socket_queue_size"`
+	MaxMessageSize  uint32        `yaml:"max_message_size"`
+	ReadBufferSize  int           `yaml:"read_buffer_size"`
+	WriteBufferSize int           `yaml:"write_buffer_size"`
+	ReadDeadline    time.Duration `yaml:"max_conn_num"`
 }
--- network/ws/config.go
+++ network/ws/config.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gonethopper/nethopper/network/common"
 )
 
-//ClientConfig grpc client config
+//ClientConfig websocket client config
 type ClientConfig struct {
 	Nodes            []common.NodeInfo `yarm:"nodes"`
 	ConnNum          int               `yarm:"conn_num"`
@@ -15,12 +15,16 @@ type ClientConfig struct {
 	MaxMessageSize   uint32            `yarm:"max_message_size"`
 	HandshakeTimeout time.Duration     `yarm:"handshake_timeout"`
 	AutoReconnect    bool              `yarm:"auto_reconnect"`
+	Token            string            `yarm:"token"`
 }
 
-//ServerConfig grpc server config
+//ServerConfig websocket server config
 type ServerConfig struct {
-	Address         string `yaml:"address"`
-	MaxConnNum      int    `yaml:"max_conn_num"`
-	SocketQueueSize int    `yaml:"socket_queue_size"`
-	MaxMessageSize  uint32 `yaml:"max_message_size"`
+	Address         string        `yaml:"address"`
+	MaxConnNum      int           `yaml:"max_conn_num"`
+	SocketQueueSize int           `yaml:"socket_queue_size"`
+	MaxMessageSize  uint32        `yaml:"max_message_size"`
+	HTTPTimeout     time.Duration `yaml:"http_timeout"`
+	CertFile        string        `yaml:"cert_file"`
+	KeyFile         string        `yaml:"key_file"`
 }

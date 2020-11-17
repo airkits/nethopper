ca966e5b3a80cff5a5203dd6aa62b77f5959f48a
admin <>
2020-11-17 23:50:13 +0800

    添加scanstruct结构

--- cache/redis/redis_feature.go
+++ cache/redis/redis_feature.go
@@ -35,6 +35,35 @@ func (c *RedisCache) HMGet(ctx context.Context, key interface{}, fieldNames ...i
 	return results, nil
 }
 
+//ScanStruct HGETALL：fields传空 HMGET: fields 按照顺序传入field
+func (c *RedisCache) ScanStruct(src []interface{}, dest interface{}, fields []string) (err error) {
+	if len(src) > 0 {
+		//redigo 返回值使用[]byte, go-redis 返回值使用string
+		//此处统一转为[]byte
+		for i := 0; i < len(src); i++ {
+			if src[i] != nil {
+				if _, ok := src[i].(string); ok {
+					src[i] = []byte(src[i].(string))
+				}
+			}
+		}
+	}
+
+	if fields != nil && len(fields) > 0 {
+		vs := make([]interface{}, 0, len(src)*2)
+		for i := 0; i < len(fields); i++ {
+			if src[i] != nil {
+				vs = append(vs, []byte(fields[i]), src[i])
+			}
+		}
+
+		err = redis.ScanStruct(vs, dest)
+	} else {
+		err = redis.ScanStruct(src, dest)
+	}
+	return err
+}
+
 //HMGetObj 获取结构体
 func (c *RedisCache) HMGetObj(ctx context.Context, out interface{}, key string, fieldNames ...string) (notFound bool, err error) {
 
@@ -46,7 +75,7 @@ func (c *RedisCache) HMGetObj(ctx context.Context, out interface{}, key string, 
 	// 	return true, nil
 	// }
 
-	return false, redis.ScanStruct(values, out)
+	return false, c.ScanStruct(values, out, fieldNames)
 }
 
 //HMSet 批量添加字段
--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gobuffalo/packr v1.30.1
 	github.com/golang/protobuf v1.4.2
-	github.com/gomodule/redigo/redis v0.0.0-20200429221454-e14091dffc1b
+	github.com/gomodule/redigo v1.8.2
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/gorilla/context v1.1.1
 	github.com/gorilla/websocket v1.4.2

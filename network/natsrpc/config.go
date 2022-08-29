9d79d2a3bf4201b18c43074f141a3786ac45cde7
ankye <shengxiehua@gmail.com>
2022-08-29 21:01:03 +0800

    更新配置中心

--- network/natsrpc/config.go
+++ network/natsrpc/config.go
@@ -4,36 +4,31 @@ import (
 	"time"
 )
 
-// ServiceInfo service node info
-type ServiceInfo struct {
-	ID      int    `mapstructure:"id"`
-	Name    string `mapstructure:"name"`
-	Subject string `mapstructure:"subject"`
-}
+const NatsServiceKey = "gameconfig"
 
 // ServiceGroup service group info
 type ServiceGroup struct {
-	ID    int           `mapstructure:"id"`
-	Name  string        `mapstructure:"name"`
-	Nodes []ServiceInfo `mapstructure:"nodes"`
-	Hash  []int         `mapstructure:"hash"`
+	Key     string `mapstructure:"key" json:"key"`
+	Type    int    `mapstructure:"type" json:"type"`
+	Version int    `mapstructure:"version" json:"version"`
+	Mode    int    `mapstructure:"mode" json:"mode"`
+	Hash    []int  `mapstructure:"hash"  json:"hash"`
 }
 
 // NatsConfig grpc client config
 type NatsConfig struct {
-	ServiceType int      `mapstructure:"service_type"`
-	ServiceID   int      `mapstructure:"service_id"`
-	Nats        []string `mapstructure:"nats"`
-	//ServiceGroup        ServiceGroup      `mapstructure:"service_group"`
-	//TargetGroup         ServiceGroup      `mapstructure:"target_group"`
-	PingInterval        time.Duration `mapstructure:"ping_interval"`
-	MaxPingsOutstanding int           `mapstructure:"max_ping_outstanding"`
-	MaxReconnects       int           `mapstructure:"max_reconnects"`
-	QueueSize           int           `mapstructure:"queue_size"`
-	SocketQueueSize     int           `mapstructure:"socket_queue_size"`
-	MaxMessageSize      uint32        `mapstructure:"max_message_size"`
-	WorkerPoolCapacity  int           `mapstructure:"worker_pool_capacity"`
-	WorkerPoolQueueSize int           `mapstructure:"worker_pool_queue_size"`
+	ServiceType         int            `mapstructure:"service_type"`
+	ServiceID           int            `mapstructure:"service_id"`
+	Nats                []string       `mapstructure:"nats"`
+	Services            []ServiceGroup `mapstructure:"services"`
+	PingInterval        time.Duration  `mapstructure:"ping_interval"`
+	MaxPingsOutstanding int            `mapstructure:"max_ping_outstanding"`
+	MaxReconnects       int            `mapstructure:"max_reconnects"`
+	QueueSize           int            `mapstructure:"queue_size"`
+	SocketQueueSize     int            `mapstructure:"socket_queue_size"`
+	AsyncMaxPending     uint32         `mapstructure:"async_max_pending"`
+	WorkerPoolCapacity  int            `mapstructure:"worker_pool_capacity"`
+	WorkerPoolQueueSize int            `mapstructure:"worker_pool_queue_size"`
 }
 
 // GetQueueSize get module queue size
